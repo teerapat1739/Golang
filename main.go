937834775fd3c2d1b8ba28f5e307c69810b2ad79
Teerapat Tee <teerapat12345678@gmail.com>
2018-10-09 19:23:17 +0700

    rest-api

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// The "/" pattern matches everything, so we need to check
+		// that we're at the root here.
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
+		}
+		fmt.Fprintf(w, "Welcome to the home page!")
+	})
+
+	http.ListenAndServe(":8080", mux)
+}
